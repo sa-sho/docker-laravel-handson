98268d33822635ee896c0975a91efb26c8a8f8fd
s-sho <sh-sato@nixe-inc.com>
2022-12-15 15:36:39 +0900

    Vue.jsのインストール後、ごはん値段計算機を動かす

--- src/app/Http/Controllers/GohanController.php
+++ src/app/Http/Controllers/GohanController.php
@@ -8,12 +8,9 @@ use App\Http\Controllers\Controller;
 class GohanController extends Controller
 {
 
-    public function index(Request $request)
+    public function index()
     {
 
-        $data['exchange'] = $request->input('rice');
-        $data['kcal'] = ($data['exchange'] * 240);
-
-        return view('gohan.index', $data);
+        return view('gohan.index');
     }
 }
--- src/resources/views/gohan/index.blade.php
+++ src/resources/views/gohan/index.blade.php
@@ -4,41 +4,123 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ごはんカロリー計算機</title>
+    <title>ごはん値段計算機</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <style>
+        [v-cloak] {
+            display: none;
+        }
+
+        .hero-body {
+            background-size: cover;
+            background-position: center;
+            background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7)), url(rice.webp);
+        }
+    </style>
 </head>
 
 <body>
     <section class="hero is-info is-fullheight has-text-centered">
 
+        <!-- ヘッダ -->
+        <div class="hero-head">
+            <header class="navbar is-primary">
+                <div class="container">
+                    <div class="navbar-brand">
+                        <a class="navbar-item is-size-4" href="">
+                            ヘッダ
+                        </a>
+                    </div>
+                </div>
+            </header>
+        </div>
+
         <!-- メインコンテンツ -->
         <div class="hero-body">
             <div class="container">
                 <div id="app" v-cloak>
-                    <div class="content mb-5">
-                        <a class="nav-link" href="{{ route('top') }}">Home</a>
+                    <div class="content">
+                        <a class="nav-link" href="{{ route('top') }}">HOME</a>
                         <h1 class="title is-size-4">
                             <i class="fa-solid fa-bowl-rice"></i>
-                            ごはんカロリー計算機
+                            ごはん値段計算機
                         </h1>
-                        <p class="is-size-7">お茶碗分のカロリーを計算します。
+                        <p class="is-size-7">ごはん一食当たりの値段とお茶碗何杯分かを計算します。数字を入力すると結果が即時反映されます。
                         </p>
                     </div>
                     <div class="container" style="width: 200px;">
                         <div class="field">
-                            <form>
-                                <input type="text" name="rice" placeholder="0杯" style="height: 30px; width: 120px;">
-                                <button type="submit">カロリーに換算</button>
-                            </form>
+                            <label class="level-right">買ったお米の量（kg）</label>
+                            <div class="control">
+                                <input class="input has-text-weight-bold has-text-right" type="number" placeholder="5"
+                                    min="1" pattern="\d*" v-model.number="weight">
+                            </div>
                         </div>
+                        <div class="field">
+                            <label class="level-right">値段（円）</label>
+                            <div class="control">
+                                <input class="input has-text-weight-bold has-text-right" type="number"
+                                    placeholder="2000" min="1" pattern="\d*" v-model.number="price">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label class="level-right">ごはん一食の量（g）</label>
+                            <div class="control">
+                                <input class="input has-text-weight-bold has-text-right" type="number" placeholder="180"
+                                    min="1" pattern="\d*" v-model.number="oneWeight">
+                            </div>
+                        </div>
+                    </div>
+                    <div id="app" class="content">
+                        <p class="is-size-5 has-text-weight-medium has-text-light">@{{ calculate }}</p>
                     </div>
                 </div>
             </div>
         </div>
-            <div class="mb-5">
-            <p>合計で{{ number_format($kcal)}}kcalです</p>
-            </div>
+
+        <!-- フッター -->
+        <div class="hero-foot">
+            <nav class="tabs is-right is-small">
+                <div class="container">
+                    <ul>
+                        <li><a href="">フッター</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@3.2.33/dist/vue.global.prod.js"></script>
+    <script>
+        const riceCalculator = {
+    data() {
+      return {
+        weight: 5,
+        price: 2000,
+        oneWeight: 180,
+      };
+    },
+    computed: {
+      calculate() {
+        if (!this.weight) {
+          return `買ったお米の量を入力してください。`;
+        }
+        if (!this.price) {
+          return `値段を入力してください。`;
+        }
+        if (!this.oneWeight) {
+          return `ごはん一食の量を入力してください。`;
+        }
+        const servings = Math.floor((this.weight * 1000 * 2.2) / this.oneWeight);
+        const servingsPrice = Math.floor(this.price / servings);
+        return `一食当たり約${servingsPrice}円、\n お茶碗${servings}杯分です。`;
+      },
+    },
+  };
+  
+  Vue.createApp(riceCalculator).mount("#app");
+    </script>
 </body>
+
 </html>
